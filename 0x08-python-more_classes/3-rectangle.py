0afadcafe58af8f0446eea6187266ddd77db7e3c
Maya1795 <mayasameh18@gmail.com>
2024-01-31 00:19:41 +0200

    bemallahhh

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,11 +50,8 @@ class Rectangle:
 
     def __str__(self):
         """return represtation of rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-            return ("".join(rect))
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                                for j in range(self.__height))
+        return string
