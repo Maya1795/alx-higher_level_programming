c31d8a014bed858366627c2e196d109a22b815c4
Maya1795 <mayasameh18@gmail.com>
2024-02-13 21:11:40 +0200

    yarab

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,8 @@ class Rectangle(Base):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
+
+    def to_dictionary(self):
+        '''dir represtnation'''
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
