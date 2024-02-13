85c4d68da4d868a6b4a4a8ec756dfd36097b3ddd
Maya1795 <mayasameh18@gmail.com>
2024-02-13 21:07:29 +0200

    besmallah

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,5 +75,26 @@ class Rectangle(Base):
 
     def __str__(self):
         '''string info'''
-        return '[{}] ({}) {}/{} - {}/{}'.format(type(self).__name__, self.id,
-                self.x, self.y, self.width, self.height)
+        return '[{}] ({}) {}/{} - {}/{}'.\
+            format(type(self).__name__, self.id, self.x, self.y, self.width,
+                   self.height)
+
+    def __update(self, id=None, width=None, height=None, x=None, y=None):
+        '''inter method for update'''
+        if id is not None:
+            self.id = id
+        if width is not None:
+            self.__width = width
+        if height is not None:
+            self.__height = height
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''update rect'''
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
