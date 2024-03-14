f31639e2796ddb7de059b9396f3af73d453b0d30
Maya1795 <mayasameh18@gmail.com>
2024-03-14 02:10:55 +0200

    besmallah

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- create user sql
-create USER IF NOT EXITS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pw
-d':
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost:
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
